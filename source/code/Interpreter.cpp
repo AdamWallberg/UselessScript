6278ebf9a10b4da97eeaff42233fe843ac79f877
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-09-07 14:12:51 +0200

    Add some code to Interpreter::run

--- source/code/Interpreter.cpp
+++ source/code/Interpreter.cpp
@@ -1,5 +1,6 @@
 #include "Interpreter.h"
 #include <assert.h>
+#include "Types.h"
 
 Interpreter::Interpreter()
 	: m_stackSize(0)
@@ -8,6 +9,39 @@ Interpreter::Interpreter()
 
 void Interpreter::run(int code[], int size)
 {
+	for (int i = 0; i < size; i++)
+	{
+		int instruction = code[i];
+
+		// TODO: Finish this shit
+		switch (instruction)
+		{
+		case Inst::INTEGER:
+		{
+			break;
+		}
+		case Inst::NEGATIVE:
+		{
+			break;
+		}
+		case Inst::ADD:
+		{
+			break;
+		}
+		case Inst::SUBTRACT:
+		{
+			break;
+		}
+		case Inst::MULTIPLY:
+		{
+			break;
+		}
+		case Inst::PRINT:
+		{
+			break;
+		}
+		}
+	}
 }
 
 void Interpreter::push(int value)
