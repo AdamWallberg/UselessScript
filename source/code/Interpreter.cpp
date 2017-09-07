217dcdf65bb83efc710e239422b684465f19b3d5
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-09-07 14:08:04 +0200

    Setup push and pop for stack

--- source/code/Interpreter.cpp
+++ source/code/Interpreter.cpp
@@ -1,4 +1,5 @@
 #include "Interpreter.h"
+#include <assert.h>
 
 Interpreter::Interpreter()
 	: m_stackSize(0)
@@ -11,10 +12,12 @@ void Interpreter::run(int code[], int size)
 
 void Interpreter::push(int value)
 {
-	
+	assert(m_stackSize <= MAX_STACK_SIZE);
+	m_stack[m_stackSize++] = value;
 }
 
 int Interpreter::pop()
 {
-	return 0;
+	assert(m_stackSize >= 0);
+	return m_stack[--m_stackSize];
 }
